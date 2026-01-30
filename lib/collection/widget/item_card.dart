931162977084b5858d2df80301c23c8cf33806f9
Habiba Khattab <habibakhattab31@gmail.com>
2026-01-30 15:32:07 +0200

    commit

--- lib/collection/widget/item_card.dart
+++ lib/collection/widget/item_card.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:nti_first_project/details/presentation/details_screen.dart';
 
 import '../../app_const/image_const.dart';
 
-// ignore: must_be_immutable
 class ItemCard extends StatelessWidget {
-   ItemCard({super.key});
+   const ItemCard({super.key});
 
-  List<Map<String, String>> getItemData = [
+  final List<Map<String, String>> getItemData = const [
     {
       'image': ImageConst.image1,
       'title': '#14415',
@@ -34,71 +34,77 @@ class ItemCard extends StatelessWidget {
     return GridView.builder(
       shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount:2,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
-          childAspectRatio: 0.75,  ),
+          childAspectRatio: 0.75,
+      ),
         itemCount: getItemData.length,
       itemBuilder: (context, index) {
         final item = getItemData[index];
-        return Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          elevation: 4,
-          child: Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Image.asset(
-                  item['image']!,
-                  height: double.infinity,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+        return InkWell(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailsScreen()));
+          },
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            elevation: 2,
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    item['image']!,
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              Positioned(
-                bottom: 5,
-                left: 8,
-                right: 8,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      item['title']!,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 3,
-                            color: Colors.black,
-                            offset: Offset(1, 1),
-                          ),
-                        ],
+                Positioned(
+                  bottom: 5,
+                  left: 8,
+                  right: 8,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        item['title']!,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 3,
+                              color: Colors.black,
+                              offset: Offset(1, 1),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 2),
-                    Text(
-                      item['price']!,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white,
-                        shadows: [
-                          Shadow(
-                            blurRadius: 3,
-                            color: Colors.black,
-                            offset: Offset(1, 1),
-                          ),
-                        ],
+                      SizedBox(height: 2),
+                      Text(
+                        item['price']!,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              blurRadius: 3,
+                              color: Colors.black,
+                              offset: Offset(1, 1),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
